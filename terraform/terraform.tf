ace51de24bd56d17f0e3cf3554b0b9209b72ca57
msulabh <msulabh76@gmail.com>
2025-02-03 00:50:19 +0530

    Updated S3 Bucket region

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -29,7 +29,7 @@ terraform {
   backend "s3" {
     bucket = "gitopsbucket18"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 
   required_version = "~> 1.6.3"
